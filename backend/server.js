1473379050718cb01b0d5a1dcc3e9e9fb8b3f62e
Kanishka-v21 <kanishkapulani17@gmail.com>
2026-07-27 17:57:11 +0530

    Authentication system Completed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes =require("./routes/authRoutes");
-
+const { protect } = require("./middleware/authMiddleware");
 dotenv.config();
 
 connectDB();
@@ -17,8 +17,12 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
     res.send("Queue Management API Running");
 });
-
+app.get("/api/profile")
 const PORT = process.env.PORT || 5000;
+
+app.get("/api/profile", protect, (req, res) => {
+    res.json(req.user);
+});
 
 app.listen(PORT, () => {
     console.log(`server Running on port ${PORT}`);
