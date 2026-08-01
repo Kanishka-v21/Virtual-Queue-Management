22fb4422abe01d3363d2a2bda1d435b628f04f37
Kanishka-v21 <kanishkapulani17@gmail.com>
2026-08-01 17:15:58 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./Context/AuthContext.jsx";
 
 import "react-toastify/dist/ReactToastify.css";
 
